1d153c796acd3ec5e56c2334f5982ee8276e5629
xpoobx <dantepellico@gmail.com>
2025-12-03 18:38:37 -0500

    corsfix2

--- server/server.js
+++ server/server.js
@@ -11,10 +11,23 @@ const applicationRoutes = require('./routes/applications.routes');
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://pet-applications-app.vercel.app' 
+];
+
 app.use(cors({
-  origin: 'https://pet-applications-app.vercel.app/', 
-  credentials: true,  
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
+
 
 app.use(bodyParser.json());
 
